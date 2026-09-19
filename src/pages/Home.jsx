1d464b7221fd128cb150885e5c7b76dfdc3a5874
Suvekshyek Nakarmi <kevusnakarmi2022@gmail.com>
2026-09-19 21:25:52 +0545

    Add product API service and connect to Home page with loading/error states

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,37 @@
+import { useEffect, useState } from "react";
+import { getAllProducts } from "../services/productService";
+
 function Home() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function fetchProduct() {
+      setLoading(true);
+      setError("");
+
+      try {
+        const data = await getAllProducts();
+        setProducts(data.products);
+      } catch (err) {
+        setError("Unable to load products. Please try again");
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchProduct();
+  }, []);
+
   return (
     <section>
-      <h1>Home Page</h1>
+      {loading && <p>Loading Products...</p>}
+      {error && <p>{error}</p>}
+      <div>
+        {products.map((p) => (
+          <li key={p.id}>{p.title}</li>
+        ))}
+      </div>
     </section>
   );
 }
---
+++ src/services/productService.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const BASE_URL = "https://dummyjson.com/products";
+
+export async function getAllProducts() {
+  const response = await axios.get(BASE_URL);
+  return response.data;
+}
+
+export async function getProductById(id) {
+  const response = await axios.get(`${BASE_URL}/${id}`);
+  return response.data;
+}
